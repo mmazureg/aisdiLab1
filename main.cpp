2e7a7e20d7998908d0e6808798c47cb2b7e28ce0
mmazureg <36950241+mmazureg@users.noreply.github.com>
2019-04-17 11:43:23 +0200

    to jest dobre

--- main.cpp
+++ main.cpp
@@ -1,9 +1,92 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <chrono>
+#include <algorithm>
+
+
 
 using namespace std;
 
+template<typename IteratorType>
+void bubbleSort(IteratorType first, IteratorType last);
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+  fstream myfile("pan-tadeusz.txt");
+
+  vector<string> dane;
+  vector<string>::iterator it;
+  string buf;
+  while(!myfile.eof())
+  {
+     myfile>>buf;
+     dane.push_back(buf);
+
+  }
+  it=dane.begin();
+  string temp;
+
+  for(it; it != dane.end(); it++)
+  {
+       temp = *it;
+       if(temp[0] >= 'A' &&  temp[0] <= 'Z' )
+       {temp[0]  += 32;}
+       *it = temp;
+
+  }
+
+auto t1 = chrono::high_resolution_clock::now();
+
+ bubbleSort(dane.begin(), dane.begin()+3000);
+
+auto t2 = chrono::high_resolution_clock::now();
+
+auto dt = chrono::duration_cast<chrono::microseconds>(t2-t1);
+cout << dt.count() << endl;
+
+
+t1 = chrono::high_resolution_clock::now();
+  sort(dane.begin(), dane.begin() + 3000);
+t2 = chrono::high_resolution_clock::now();
+dt = chrono::duration_cast<chrono::microseconds>(t2-t1);
+
+cout << dt.count() << endl;
+
+
+
+
 }
+
+template<typename IteratorType>
+void bubbleSort(IteratorType first, IteratorType last)
+{
+
+  IteratorType it1 = first;
+
+
+  int lenght = last - first;
+
+
+
+  for(int i = 0; i<lenght; i++)
+  {
+    for(first = it1; first != last-i-1; first++)
+    {
+
+         if((*first) > *(first+1))
+         {  swap(*first, *(first+1)); }
+
+    }
+
+  }
+
+
+
+
+}
+
+
+
+
